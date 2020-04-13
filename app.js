c6c98a0e40772f484714b122510cd86ac589b57c
Bohdan Asauliuk <bohdan.asauliuk@gmail.com>
2020-04-13 14:32:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const navbar = document.getElementById("navbar");
+const navList = document.querySelector(".header-nav__list");
+const sidebar = document.getElementById("sidebar");
+const btn = document.getElementById("menu-button");
+const sideBtn = document.getElementById("side-button");
+const open = document.querySelector(".open");
+const links = document.querySelectorAll('.header-nav__item-link');
+
+
+function hideNavBar() {
+  navbar.classList.toggle("open");
+  btn.classList.toggle("transform");
+  navList.classList.toggle("transform-list");
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault;
+  hideNavBar();
+});
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    hideNavBar();
+  });
+}
+
+sideBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  sidebar.classList.toggle("open");
+  sideBtn.classList.toggle("transform-side");
+});
+
+// Style the buy button 
+const animateButton = function (e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+const bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (let i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+// Slider
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+
+}
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  }
+}
+
+// when I click left, move slides to the left
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevButton, nextButton, prevIndex);
+
+});
+
+// when I click right, move slides to the right
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide)
+
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevButton, nextButton, nextIndex);
+
+});
+
+// when I click the nav indicators, move to that slide 
+dotsNav.addEventListener('click', e => {
+  // what indicator was clicked on?
+  const targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+
+  hideShowArrows(slides, prevButton, nextButton, targetIndex);
+});
