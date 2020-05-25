9a97656a929c3a6cccaf91fca8778fec0cda0bbd
Bohdan Asauliuk <bohdan.asauliuk@gmail.com>
2020-05-25 14:21:02 +0200

    add jquery carousel

--- app.js
+++ app.js
@@ -5,156 +5,145 @@ const sideContent = document.querySelector(".sidebar__container");
 const btn = document.getElementById("menu-button");
 const sideBtn = document.getElementById("side-button");
 const open = document.querySelector(".open");
-const links = document.querySelectorAll('.header-nav__item-link');
-
+const links = document.querySelectorAll(".header-nav__item-link");
 
 // datepicker
 $(function () {
   $("#datepicker").datepicker({
     inline: true,
     showOtherMonths: true,
-    dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
   });
-});
 
-function hideNavBar() {
-  navbar.classList.toggle("open");
-  btn.classList.toggle("transform");
-  navList.classList.toggle("transform-list");
-}
+  function hideNavBar() {
+    navbar.classList.toggle("open");
+    btn.classList.toggle("transform");
+    navList.classList.toggle("transform-list");
+  }
 
-function hideSideBar() {
-  sidebar.classList.toggle("open");
-  sideBtn.classList.toggle("transform-side");
-  sideContent.classList.toggle("transform-sidebar");
-}
+  function hideSideBar() {
+    sidebar.classList.toggle("open");
+    sideBtn.classList.toggle("transform-side");
+    sideContent.classList.toggle("transform-sidebar");
+  }
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault;
-  hideNavBar();
-});
-
-for (const link of links) {
-  link.addEventListener("click", () => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault;
     hideNavBar();
   });
-}
 
-sideBtn.addEventListener("click", (e) => {
-  e.preventDefault;
-  hideSideBar();
-});
-
-// Style the buy button 
-const animateButton = function (e) {
-
-  e.preventDefault;
-  //reset animation
-  e.target.classList.remove('animate');
-
-  e.target.classList.add('animate');
-  setTimeout(function () {
-    e.target.classList.remove('animate');
-  }, 700);
-};
-
-const bubblyButtons = document.getElementsByClassName("bubbly-button");
-
-for (let i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener('click', animateButton, false);
-}
-
-// Slider
-const track = document.querySelector(".carousel__track");
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
-
-const slideWidth = slides[0].getBoundingClientRect().width;
-
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
-};
-
-slides.forEach(setSlidePosition);
-
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = `translateX(-${targetSlide.style.left})`;
-  currentSlide.classList.remove('current-slide');
-  targetSlide.classList.add('current-slide');
-
-}
-
-const updateDots = (currentDot, targetDot) => {
-  currentDot.classList.remove('current-slide');
-  targetDot.classList.add('current-slide');
-};
-
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-  if (targetIndex === 0) {
-    prevButton.classList.add('is-hidden');
-    nextButton.classList.remove('is-hidden');
-  } else if (targetIndex === slides.length - 1) {
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.add('is-hidden');
-  } else {
-    prevButton.classList.remove('is-hidden');
-    nextButton.classList.remove('is-hidden');
+  for (const link of links) {
+    link.addEventListener("click", () => {
+      hideNavBar();
+    });
   }
-}
 
-// when I click left, move slides to the left
-prevButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  const prevSlide = currentSlide.previousElementSibling;
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const prevDot = currentDot.previousElementSibling;
-  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+  sideBtn.addEventListener("click", (e) => {
+    e.preventDefault;
+    hideSideBar();
+  });
 
-  moveToSlide(track, currentSlide, prevSlide);
-  updateDots(currentDot, prevDot);
-  hideShowArrows(slides, prevButton, nextButton, prevIndex);
+  // Style the buy button
+  const animateButton = function (e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove("animate");
 
-});
+    e.target.classList.add("animate");
+    setTimeout(function () {
+      e.target.classList.remove("animate");
+    }, 700);
+  };
 
-// when I click right, move slides to the right
-nextButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  const nextSlide = currentSlide.nextElementSibling;
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const nextDot = currentDot.nextElementSibling;
-  const nextIndex = slides.findIndex(slide => slide === nextSlide)
+  const bubblyButtons = document.getElementsByClassName("bubbly-button");
 
-  moveToSlide(track, currentSlide, nextSlide);
-  updateDots(currentDot, nextDot);
-  hideShowArrows(slides, prevButton, nextButton, nextIndex);
+  for (let i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener("click", animateButton, false);
+  }
 
-});
+  // JQUERY CAROUSEL
+  const track = $(".carousel__track");
+  const slides = track.children().toArray();
+  const nextButton = $(".carousel__button--right");
+  const prevButton = $(".carousel__button--left");
+  const dotsNav = $(".carousel__nav");
+  const dots = dotsNav.children().toArray();
 
-// when I click the nav indicators, move to that slide 
-dotsNav.addEventListener('click', e => {
-  // what indicator was clicked on?
-  const targetDot = e.target.closest('button');
+  const slideWidth = slides[0].getBoundingClientRect().width;
 
-  if (!targetDot) return;
+  $.each(slides, (index, slide) => {
+    $(slide).css("left", `${slideWidth * index}px`);
+  });
 
+  function moveToSlide(track, currentSlide, targetSlide) {
+    $(track).css("transform", `translateX(-${$(targetSlide).css("left")})`);
 
-  const currentSlide = track.querySelector('.current-slide');
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const targetIndex = dots.findIndex(dot => dot === targetDot);
-  const targetSlide = slides[targetIndex];
+    $(currentSlide).removeClass("current-slide");
+    $(targetSlide).addClass("current-slide");
+  }
 
+  function updateDots(currentDot, targetDot) {
+    $(currentDot).removeClass("current-slide");
+    $(targetDot).addClass("current-slide");
+  }
 
-  moveToSlide(track, currentSlide, targetSlide);
-  updateDots(currentDot, targetDot);
+  function hideShowArrows(slides, prevButton, nextButton, targetIndex) {
+    if (targetIndex === 0) {
+      $(prevButton).addClass("is-hidden");
+      $(nextButton).removeClass("is-hidden");
+    } else if (targetIndex === slides.length - 1) {
+      $(prevButton).removeClass("is-hidden");
+      $(nextButton).addClass("is-hidden");
+    } else {
+      $(prevButton).removeClass("is-hidden");
+      $(nextButton).removeClass("is-hidden");
+    }
+  }
 
-  hideShowArrows(slides, prevButton, nextButton, targetIndex);
-});
+  // when I click left, move slides to the left
+  prevButton.click((e) => {
+    const currentSlide = track.find(".current-slide");
+    const prevSlide = currentSlide.prev();
+    const currentDot = dotsNav.find(".current-slide");
+    const prevDot = currentDot.prev();
+    const prevIndex = prevSlide.index();
 
-// add autocomplete
-$(function () {
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+  });
+
+  // when I click right, move slides to the right
+  nextButton.click((e) => {
+    const currentSlide = track.find(".current-slide");
+    const nextSlide = currentSlide.next();
+    const currentDot = dotsNav.find(".current-slide");
+    const nextDot = currentDot.next();
+    const nextIndex = nextSlide.index();
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+  });
+
+  // when I click the nav indicators, move to that slide
+  dotsNav.click((e) => {
+    const targetDot = $(e.target).closest("button");
+
+    if (!targetDot) return;
+
+    const currentSlide = track.find(".current-slide");
+    const currentDot = dotsNav.find(".current-slide");
+    const targetIndex = targetDot.index();
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+  });
+
+  // autocomplete
   var availableTags = [
     "3T",
     "6ku",
@@ -180,9 +169,9 @@ $(function () {
     "Univega",
     "Vilano",
     "Whyte",
-    "Zinn Cycles"
+    "Zinn Cycles",
   ];
   $("#tags").autocomplete({
-    source: availableTags
+    source: availableTags,
   });
 });
